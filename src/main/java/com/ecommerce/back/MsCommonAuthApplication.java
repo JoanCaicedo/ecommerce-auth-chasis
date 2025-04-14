a7bab5cad287df8636c6b90f828c30c8dbe555d2
joancaicedo <jaca.caicedo@gmail.com>
2025-04-14 15:47:11 -0500

    First load to Chasis

---
+++ src/main/java/com/ecommerce/back/MsCommonAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCommonAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCommonAuthApplication.class, args);
+	}
+
+}
