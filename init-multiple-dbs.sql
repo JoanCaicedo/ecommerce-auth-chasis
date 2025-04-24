53949ead7f38a9b31505ddfc12b0a51f6bd23710
JoanCaicedo <94801062+JoanCaicedo@users.noreply.github.com>
2025-04-24 15:33:02 -0500

    Feature/configuration basic (#4)
    * add the db postgres generator
    
    * add login to connect to DB

---
+++ init-multiple-dbs.sql
@@ -0,0 +1,9 @@
+-- Script para crear bases de datos por microservicio
+
+CREATE DATABASE ecommerce_auth_db;
+CREATE DATABASE ecommerce_product_db;
+CREATE DATABASE ecommerce_order_db;
+CREATE DATABASE ecommerce_cart_db;
+CREATE DATABASE ecommerce_payment_db;
+CREATE DATABASE ecommerce_notification_db;
+CREATE DATABASE ecommerce_chasis_db;
