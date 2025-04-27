0f26a248f009262a79e15c749f656481725560db
AlBenR <abenavides.restrepo@gmail.com>
2025-04-26 21:32:17 -0500

    added update to CRUD

--- src/main/java/com/ecommerce/back/application/service/BaseReferenceService.java
+++ src/main/java/com/ecommerce/back/application/service/BaseReferenceService.java
@@ -4,7 +4,6 @@ package com.ecommerce.back.application.service;
 import com.ecommerce.back.domain.model.BaseReference;
 import com.ecommerce.back.domain.ports.BaseReferenceRepositoryPort;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
