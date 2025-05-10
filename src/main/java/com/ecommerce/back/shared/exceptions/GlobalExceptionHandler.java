2b7b6f6d844b58c7a685de42bbcab838ae912ba1
joancaicedo <jaca.caicedo@gmail.com>
2025-05-10 16:47:47 -0500

    add new exception component and https codes

--- src/main/java/com/ecommerce/back/shared/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/back/shared/exceptions/GlobalExceptionHandler.java
@@ -9,15 +9,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(GeneralException.class)
-    public ResponseEntity<ErrorResponse> handleException(final Exception exception, HttpServletRequest request) {
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<ErrorResponse> handleValidation(final Exception exception, HttpServletRequest request) {
         ErrorResponse response = new ErrorResponse(exception.getMessage(), request.getRequestURI());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body(response);
+    }
+
+    @ExceptionHandler(GeneralException.class)
+    public ResponseEntity<ErrorResponse> handleGeneral(final Exception exception, HttpServletRequest request) {
+        ErrorResponse response = new ErrorResponse(exception.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(response);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleGeneral(HttpServletRequest request) {
+    public ResponseEntity<ErrorResponse> handleException(HttpServletRequest request) {
         ErrorResponse response = new ErrorResponse("Internal server error", request.getRequestURI());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 }
